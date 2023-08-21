83de3d297a63dd587c6f70e6321a7f9134835e36
warningfm <warningfm@gmail.com>
2023-08-22 00:07:43 +0700

    Update lunaradio-sincors.js

--- js/lunaradio-sincors.js
+++ js/lunaradio-sincors.js
@@ -456,7 +456,7 @@ if (w.metadatatechnic === "corsproxy") {
                 dataType: "text",
                 method: "GET",
                 crossDomain: !0,
-                url: "https://zenoplay.zenomedia.com/api/zenofm/nowplaying/"+ zn +"?_=",
+                url: "https://zenoplay.zenomedia.com/api/zenofm/nowplaying/" + zn + "?_= ",
                 success: function(b) {
                     try {
                         var a = JSON.parse(b);
